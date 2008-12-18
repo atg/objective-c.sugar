7f2122e59a8f2b62a6506f3d9480188c849c0720
Alex Gordon <anything@fileability.net>
2008-12-18 15:38:43 +0000

    First commit

---
+++ TestFile.m
@@ -0,0 +1,67 @@
+#import <test>
+#include "test"
+#pragma mark asd
+#warning test
+#define test(a) a*2
+
+#if (x == 1)
+#elif (x == 2)
+#endif
+q
+#ifdef test
+#endif
+
+/*
+	Comment A
+	Comment B	
+*/
+
+void a(int *mk) {
+	
+	
+}
+
+void foo(asd asd)
+{
+
+}
+
+@interface NSObject : test
+	void foo(asd asd)
+{
+
+}
+
+- testa:(asd <asd>)a testa:(asd)a
+{
+
+}
+
+@end
+
+
+
+
+//Line comment
+{
+
+}
+if
+
+42
+10.634
+123e-23
+0x123456
+01234
+
+"A string"
+"A string with \n newlines and \t tabs"
+'b'
+
+int x = 5;
+register uint y = 2;
+
+bool b = true;
+
+printf("abc %d", x);
+scanf("%u", &y);
